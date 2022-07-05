c1032f11e01c8bdccf18a4ff091c2a77485adb44
Your Name <tpunchihewage@gmail.com>
2022-07-05 15:37:04 +0200

    added Data to solutions.js

--- solution.js
+++ solution.js
@@ -1,1 +1,22 @@
+// Exercise - 01
 
+console.log("1.1", isNaN(" hello"));
+
+console.log("1.2", isNaN(3));
+
+console.log(
+  "1.3",
+  "it is a numeric data type whose value cannot be represented using actual numbers."
+);
+
+// Exercise - 02
+
+console.log("2.1", (0.1 * 0.2).toFixed(2));
+
+// Exercise - 03
+
+console.log("3.1", Infinity / 0);
+
+console.log("3.2", Infinity / Infinity);
+
+console.log("3.3", 1 / 0);
